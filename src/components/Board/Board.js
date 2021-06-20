9703ddbd1ecb1a049213c8ad08a370b53234bf6e
Mamata Barman <mamata.android@gmail.com>
2021-06-21 02:21:23 +0530

    fix: Board function modified

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,9 +2,7 @@ import React from 'react'
 import './Board.css';
 import Tile from "../Tile";
 const Board = (props) => {
-  const tiles=props.tiles.map(tile=>{
-    <Tile {...tile}/>
-  })
+  const tiles=props.tiles.map(tile=>(<Tile {...tile}/>));
 
   const gridConfig = {
     gridTemplateColumns: `repeat(${Math.sqrt(props.numTiles)}, 1fr)`,
