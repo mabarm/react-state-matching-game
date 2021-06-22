17417ea956e2b5c3e8bb6d81246ee6e885fe0689
Mamata Barman <mamata.android@gmail.com>
2021-06-22 18:51:34 +0530

    feat : handling a matched tile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -30,9 +30,18 @@ class App extends Component{
         let toBeCleared = state.toBeCleared;
         const selectedTileIndex = indexOfSelected(tiles,id,color);
         let previousTileIndex = state.previousTileIndex;
-        if(previousTileIndex===null){
+        if(previousTileIndex!==null){
+          previousTile = tiles[previousTileIndex];
+          selectedTile = tiles[selectedTileIndex];
+          if(previousTile.id !== selectedTile.id && previousTile.color === selectedTile.color){
+              selectedTile.matched = true;
+              previousTile.matched = true;
+              previousTileIndex = null;
+          }
+        }else{
           previousTileIndex = selectedTileIndex;
         }
+      
         return { tiles,toBeCleared,previousTileIndex }
       }) 
     }
